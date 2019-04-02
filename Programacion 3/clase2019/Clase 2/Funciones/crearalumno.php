1199aae7f43c3b97a2618ede1867847b5af38642
Your Name <you@example.com>
2019-04-01 22:16:36 -0300

    a

--- Programacion 3/clase2019/Clase 2/Funciones/crearalumno.php
+++ Programacion 3/clase2019/Clase 2/Funciones/crearalumno.php
@@ -17,4 +17,50 @@ echo $alumnoPost->jsonReturn();
 //var_dump($alumnoPost);
 $alumnoPost->guardarAlumno($path);
 $alumnoPost->guardarAlumnoJson($pathjson);
+
+//------------------------------------------------- FOTOS ----------------------------------////
+
+$file = $_FILES['archivo'];
+$filemarca = $_FILES['marca'];
+
+$marca = $filemarca["name"];
+$marcatmp = $filemarca["tmp_name"];
+var_dump($file);
+
+$filetmpname = $file["tmp_name"];
+$filename = $file["name"];
+$filecutname=explode(".",$filename);
+
+
+
+
+
+
+if(file_exists("./fotos/$filename"))
+{
+    move_uploaded_file($marcatmp,"./fotos/$marca");
+    $margen_dcho = 10;
+    $margen_inf = 10;
+    $sx = imagesx("./fotos/$marca");
+    $sy = imagesy("./fotos/$marca");
+    imagecopy($filename, $marca, imagesx($im) 
+    - $sx - $margen_dcho, imagesy($filename) - $sy - $margen_inf, 0, 0, imagesx($marca), imagesy($marca));
+    $dia = getdate();
+    $filecutname=explode(".",$filename);
+    $oldpath = "./fotos/$filename";
+    var_dump($oldpath);
+    $newpath = "./fotosbackup/$filecutname[0]". $dia["mday"]."-". $dia["mon"] . "-" . $dia["year"] . "." .$filecutname[1];
+    var_dump($newpath);
+    rename($oldpath,$newpath);
+    move_uploaded_file($filetmpname,"./fotos/$filename");
+}
+else
+{
+
+    move_uploaded_file($filetmpname,"./fotos/$filename");
+}
+
+
+
+
 ?>
